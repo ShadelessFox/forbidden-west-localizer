8db4c3611ca5810e5d58a311c40fa7d7f49b0d25
ShadelessFox <yanuari.channel@gmail.com>
2025-06-17 23:10:01 +0300

    Migrate to QuickDLLProxy

--- source/dllmain.cpp
+++ source/dllmain.cpp
@@ -1,12 +1,16 @@
-#include <Windows.h>
+#define DLL_PROXY_EXPORT_LISTING_FILE "exports.inc"             // List of exported functions
+#define DLL_PROXY_CHECK_MISSING_EXPORTS                         // Strict validation
+#define DLL_PROXY_DECLARE_IMPLEMENTATION                        // Then define the whole implementation
+#include <QuickDllProxy/DllProxy.h>
 
 #include "Injector/Injector.h"
 
-[[maybe_unused]] BOOL WINAPI DllMain(HINSTANCE instance, DWORD reason, LPVOID *reserved) {
+[[maybe_unused]] BOOL APIENTRY DllMain(HINSTANCE instance, DWORD reason, LPVOID *reserved) {
     (void) instance;
     (void) reserved;
 
     if (reason == DLL_PROCESS_ATTACH) {
+        DllProxy::Initialize();
         Injector::Attach();
     }
 
